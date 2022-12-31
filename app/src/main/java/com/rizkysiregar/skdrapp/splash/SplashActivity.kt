afc948b9bb177d55e71b1cb3477ad7f9e7cb3d24
Habib Rizky Almajid Siregar <regarrezky@gmail.com>
2022-12-31 10:53:44 +0700

    create bottom navigation view

--- app/src/main/java/com/rizkysiregar/skdrapp/splash/SplashActivity.kt
+++ app/src/main/java/com/rizkysiregar/skdrapp/splash/SplashActivity.kt
@@ -1,8 +1,12 @@
 package com.rizkysiregar.skdrapp.splash
 
 import android.annotation.SuppressLint
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.os.Handler
+import android.os.Looper
+import com.rizkysiregar.skdrapp.MainActivity
 import com.rizkysiregar.skdrapp.R
 
 @SuppressLint("CustomSplashScreen")
@@ -10,5 +14,14 @@ class SplashActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_splash)
+
+        Handler(Looper.getMainLooper()).postDelayed({
+            startActivity((Intent(this, MainActivity::class.java)))
+            finish()
+        }, FAKE_LOAD_TIME)
+    }
+
+    companion object {
+        private const val FAKE_LOAD_TIME: Long = 3000;
     }
 }
