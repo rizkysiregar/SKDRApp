487e88ca24cea08fe8ad966713ab17c9351dc2b2
Habib Rizky Almajid Siregar <regarrezky@gmail.com>
2023-01-17 21:34:06 +0700

    Update RecapitulationFragment.kt
    add fitur untuk kirim laporan ke wa dan sms

--- app/src/main/java/com/rizkysiregar/skdrapp/recapitulation/RecapitulationFragment.kt
+++ app/src/main/java/com/rizkysiregar/skdrapp/recapitulation/RecapitulationFragment.kt
@@ -1,5 +1,7 @@
 package com.rizkysiregar.skdrapp.recapitulation
 
+import android.content.Intent
+import android.net.Uri
 import android.os.Bundle
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
@@ -68,6 +70,47 @@ class RecapitulationFragment : Fragment() {
             }
             binding.tvFormatLaporan.text = sb
         }
+        var number = "6281279648621"
+        binding.btnWa.setOnClickListener {
+           sendWA(sb,number)
+        }
+
+        binding.btnSms.setOnClickListener {
+            sendSms(sb.toString(),number)
+        }
+
+    }
+
+    private fun sendWA(sb: StringBuilder, number: String){
+        try {
+            val sendIntent = Intent().apply {
+                action = Intent.ACTION_SEND
+                putExtra(Intent.EXTRA_TEXT, sb.toString())
+                putExtra("jid", "${number}@s.whatsapp.net")
+                type = "text/plain"
+                setPackage("com.whatsapp")
+            }
+            startActivity(sendIntent)
+        }catch (e: Exception){
+            e.printStackTrace()
+            val appPackageName = "com.whatsapp"
+            try {
+                startActivity(Intent(Intent.ACTION_VIEW, Uri.parse("market://details?id=$appPackageName")))
+            }catch (e: android.content.ActivityNotFoundException){
+                startActivity(Intent(Intent.ACTION_VIEW, Uri.parse("https://play.google.com/store/apps/details?id=$appPackageName")))
+            }
+        }
+    }
+
+    private fun sendSms(message: String, phone: String){
+        val uri = Uri.parse("smsto:+$phone")
+        val intent = Intent(Intent.ACTION_SENDTO, uri)
+
+        with(intent){
+            putExtra("address", "+$phone")
+            putExtra("sms_body", message)
+        }
+        startActivity(intent)
     }
 
 
