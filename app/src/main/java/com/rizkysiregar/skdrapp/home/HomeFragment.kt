9ff294aa285575ebc4c213c2b6b61f0266e7b2e6
Habib Rizky Almajid Siregar <regarrezky@gmail.com>
2023-02-04 20:33:09 +0700

    add empty data condition

--- app/src/main/java/com/rizkysiregar/skdrapp/home/HomeFragment.kt
+++ app/src/main/java/com/rizkysiregar/skdrapp/home/HomeFragment.kt
@@ -1,26 +1,17 @@
 package com.rizkysiregar.skdrapp.home
 
 import android.content.Intent
-import android.graphics.Color
-import android.graphics.Typeface
+import android.opengl.Visibility
 import android.os.Bundle
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import android.widget.ArrayAdapter
-import androidx.core.content.ContextCompat
 import androidx.recyclerview.widget.LinearLayoutManager
-import com.github.mikephil.charting.animation.Easing
 import com.github.mikephil.charting.charts.PieChart
-import com.github.mikephil.charting.data.PieData
-import com.github.mikephil.charting.data.PieDataSet
-import com.github.mikephil.charting.data.PieEntry
-import com.github.mikephil.charting.formatter.PercentFormatter
-import com.github.mikephil.charting.utils.MPPointF
 import com.rizkysiregar.skdrapp.R
 import com.rizkysiregar.skdrapp.add.AddDataActivity
-import com.rizkysiregar.skdrapp.core.domain.model.Skdr
 import com.rizkysiregar.skdrapp.core.ui.SkdrAdapter
 import com.rizkysiregar.skdrapp.databinding.FragmentHomeBinding
 import org.koin.androidx.viewmodel.ext.android.viewModel
@@ -81,8 +72,12 @@ class HomeFragment : Fragment() {
         }
         homeViewModel.skdr.observe(requireActivity()){
             skdrAdapter.setData(it)
+            if (it.isNullOrEmpty()){
+                binding.tvEmpty.visibility = View.VISIBLE
+            }else{
+                binding.tvEmpty.visibility = View.GONE
+            }
         }
-
     }
 
     override fun onDestroyView() {
