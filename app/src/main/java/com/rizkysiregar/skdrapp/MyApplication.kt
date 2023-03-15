09dc59b61b0aebc7fa05c53e1e46afa1ceddeea3
Habib Rizky Almajid Siregar <regarrezky@gmail.com>
2023-03-15 17:58:18 +0700

    Update MyApplication.kt

--- app/src/main/java/com/rizkysiregar/skdrapp/MyApplication.kt
+++ app/src/main/java/com/rizkysiregar/skdrapp/MyApplication.kt
@@ -11,9 +11,16 @@ import org.koin.core.context.startKoin
 import org.koin.core.logger.Level
 import org.koin.dsl.module
 
+// extend to Application
 class MyApplication : Application() {
     override fun onCreate() {
         super.onCreate()
+        
+        /*
+        *   startKoin will be need higher context of application
+        *   and pass all modules that will be take care of koin library
+        *   to provide context and inject value in field who need them
+        * */
         startKoin {
             androidLogger()
             androidContext(this@MyApplication)
