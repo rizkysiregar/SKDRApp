a1bb6b76396975881aa5eb009fe222a669f9801f
Habib Rizky Almajid Siregar <regarrezky@gmail.com>
2023-03-15 17:28:46 +0700

    Update MapsViewModel.kt
    comment

--- app/src/main/java/com/rizkysiregar/skdrapp/maps/MapsViewModel.kt
+++ app/src/main/java/com/rizkysiregar/skdrapp/maps/MapsViewModel.kt
@@ -9,15 +9,22 @@ import com.rizkysiregar.skdrapp.core.domain.model.Skdr
 import com.rizkysiregar.skdrapp.core.domain.usecase.SkdrUseCase
 
 class MapsViewModel(skdrUseCase: SkdrUseCase): ViewModel() {
+
+    // get all data from db using skdrUseCase
     val getAllData = skdrUseCase.getAllData()
 
+    // mutable live data for LiveData purpose
     private val _getDataByNamaDesa = MutableLiveData<String>()
+
+    // switch map
     private val _skdr = _getDataByNamaDesa.switchMap { namaDesa ->
         skdrUseCase.getAllDataByNamaDesa(namaDesa)
     }
 
+    // create variable that obtain value from _skdr
     val skdr: LiveData<List<Skdr>> = _skdr
 
+    // set value for _getAllDataByNamaDesa
     fun setNamaDesa(namaDesa: String) {
         if (namaDesa == _getDataByNamaDesa.value){
             return
