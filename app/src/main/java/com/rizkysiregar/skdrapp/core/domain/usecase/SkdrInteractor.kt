668cf29425d19b4e60d7536a856ff758ff82f2d8
Habib Rizky Almajid Siregar <regarrezky@gmail.com>
2023-03-15 09:31:31 +0700

    Update SkdrInteractor.kt
    Comment

--- app/src/main/java/com/rizkysiregar/skdrapp/core/domain/usecase/SkdrInteractor.kt
+++ app/src/main/java/com/rizkysiregar/skdrapp/core/domain/usecase/SkdrInteractor.kt
@@ -4,6 +4,13 @@ import androidx.lifecycle.LiveData
 import com.rizkysiregar.skdrapp.core.domain.model.Skdr
 import com.rizkysiregar.skdrapp.core.domain.repository.ISkdrRepository
 
+/*
+    extends to SkdrUseCase and override all method on that interface,
+    this class need skdrRepository interface as constructor that
+    has been inject () by dependency injection Koin in AppModule()
+    can find in folder : app/core/AppModule
+*/
+
 class SkdrInteractor(private val skdrRepository: ISkdrRepository): SkdrUseCase {
     override fun getAllData(): LiveData<List<Skdr>> = skdrRepository.getAllData()
     override fun insertNewData(skdr: Skdr) = skdrRepository.insertData(skdr)
