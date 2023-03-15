41eb3d94763f0a58377a7d1064c58052a0ddb309
Habib Rizky Almajid Siregar <regarrezky@gmail.com>
2023-03-15 09:42:59 +0700

    Update SkdrDatabase.kt
    comment

--- app/src/main/java/com/rizkysiregar/skdrapp/core/data/room/SkdrDatabase.kt
+++ app/src/main/java/com/rizkysiregar/skdrapp/core/data/room/SkdrDatabase.kt
@@ -5,6 +5,12 @@ import androidx.room.RoomDatabase
 import com.rizkysiregar.skdrapp.core.data.entity.DataPenyakitEntity
 import com.rizkysiregar.skdrapp.core.data.entity.SkdrEntity
 
+/*
+*
+* Database initialisation with Room SQLite Library which
+* contain database name, database data class that used to be table
+* add call skdrDao for custom query to database
+* */
 @Database(entities = [SkdrEntity::class, DataPenyakitEntity::class], version = 2, exportSchema = false)
 abstract class SkdrDatabase: RoomDatabase() {
     abstract fun skdrDao(): SkdrDao
